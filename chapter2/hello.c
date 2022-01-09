0b04c33c7e0e7576e91f1394c28186d35892f529
hjh0915 <623299157@qq.com>
2022-01-09 21:05:06 +0800

    程序编译与运行

---
+++ chapter2/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int f_sum(int a, int b);
+
+int main() {
+    int var_sum = 0;
+    printf("hello world!\n");
+    var_sum = f_sum(2, 3);
+    
+    return var_sum;
+}
---
+++ chapter2/macro.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define NUM 5
+
+int main(void) {
+    printf("NUM的值是%d\n", NUM);
+    
+    return 0;
+}
